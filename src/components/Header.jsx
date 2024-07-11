030df8469ee0ad05045b9f503ccc7581e839a78f
jeya-chandran-jcs <jeyachandranjcs@gmail.com>
2024-07-12 02:01:12 +0530

    sync

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,30 +1,41 @@
 import React from 'react';
 import { Navbar, Nav, Container } from 'react-bootstrap';
-import { Link } from 'react-router-dom';
-import { Link as ScrollLink } from 'react-scroll';
-import { Icon } from '@iconify/react';
-import sunIcon from '@iconify-icons/mdi/white-balance-sunny';
-import moonIcon from '@iconify-icons/mdi/moon-waning-crescent';
+import { Link } from 'react-scroll'; 
 import './header.css';
 
 const Header = () => {
   return (
-    <Navbar expand="lg"  className="mb-4 sticky-top z-1 navBar">
-      <Container >
-        <Navbar.Brand href="/">Jeya chandran s</Navbar.Brand>
+    <Navbar expand="lg" className="mb-4 sticky-top z-1 navBar">
+      <Container>
+        <Navbar.Brand ><Link to="home" smooth={true} duration={100}>Jeya Chandran S</Link></Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ml-auto position-relative">
-            <div className="cursor-line"></div> 
-            <Nav.Link as={Link} to="/home" className="active-link">Home</Nav.Link>
-            <Nav.Link as={Link} to="/about" className="active-link">About</Nav.Link>
-            <Nav.Link as={Link} to="/skills" className="active-link">Skills</Nav.Link>
-            <Nav.Link as={Link} to="/projects" className="active-link">Projects</Nav.Link>
-            <Nav.Link as={Link} to="/education" className="active-link">Education</Nav.Link>
-            <Nav.Link as={Link} to="/intern" className="active-link">Intern</Nav.Link>
-            <Nav.Link as={Link} to="/resume" className="active-link">Resume</Nav.Link>
-            <Nav.Link as={Link} to="/contact" className="active-link">Contact</Nav.Link>
-          
+            <div className="cursor-line"></div>
+            <Nav.Link>
+              <Link to="home" smooth={true} duration={100} className="active-link">Home</Link>
+            </Nav.Link>
+            <Nav.Link>
+              <Link to="about" smooth={true} duration={100} className="active-link">About</Link>
+            </Nav.Link>
+            <Nav.Link>
+              <Link to="skills" smooth={true} duration={100}  className="active-link">Skills</Link>
+            </Nav.Link>
+            <Nav.Link>
+              <Link to="projects" smooth={true} duration={100} className="active-link">Projects</Link>
+            </Nav.Link>
+            <Nav.Link>
+              <Link to="education" smooth={true} duration={100} className="active-link">Education</Link>
+            </Nav.Link>
+            <Nav.Link>
+              <Link to="intern" smooth={true} duration={100} className="active-link">Intern</Link>
+            </Nav.Link>
+            <Nav.Link>
+              <Link to="resume" smooth={true} duration={100} className="active-link">Resume</Link>
+            </Nav.Link>
+            <Nav.Link>
+              <Link to="contact" smooth={true} duration={100} className="active-link">Contact</Link>
+            </Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -58,18 +58,19 @@
 // export default Home;
 import React, { useEffect, useState } from 'react';
 import AOS from 'aos';
+import { Link } from 'react-scroll';
 import { Container, Row, Col } from 'react-bootstrap';
 import { FaGithub, FaLinkedin } from 'react-icons/fa';
 import 'aos/dist/aos.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './home.css';
-import { useNavigate } from 'react-router-dom';
 
 AOS.init();
 
 const Home = () => {
   const [currentTitle, setCurrentTitle] = useState(0);
-  const titles = ['Web Developer', 'MERN Stack Developer', 'Frontend Developer'];
+  const titles = ['Web Developer', 'MERN Stack Developer', 'Frontend Enthusiast', 'Backend Architect'];
+
 
   useEffect(() => {
     const intervalId = setInterval(() => {
@@ -79,7 +80,7 @@ const Home = () => {
     return () => clearInterval(intervalId);
   }, []);
 
-  const navigate = useNavigate();
+ 
 
   return (
     <div className="container-fluid home-container">
@@ -87,9 +88,11 @@ const Home = () => {
         <Col xs={12} md={6} className="text-center" data-aos="fade-right">
           <h1 className="text-dark cursive text1">Hi, I'm</h1>
           <h1 className="text-primary text2">Jeya Chandran</h1>
-          <h2 className="text3 m-2">{titles[currentTitle]}</h2>
+          <h2 className="text3 mt-3 fw-bold">{titles[currentTitle]}</h2>
           <div className="d-flex justify-content-center align-items-center gap-2 mt-3">
-            <button className="btn btn-outline-primary btn-lg" onClick={() => navigate('/contact')} data-aos="fade-left">Contact</button>
+            <button className="btn btn-outline-primary btn-lg"  data-aos="fade-left">
+              <Link to="contact" activeClass="active" spy={true} smooth={true} offset={50} 
+              duration={100}>Contact</Link></button>
             <a href="https://github.com/jeya-chandran-jcs" target="_blank" rel="noopener noreferrer" className="icon-link" data-aos="fade-up">
               <FaGithub size={40} />
             </a>
@@ -98,7 +101,7 @@ const Home = () => {
             </a>
           </div>
         </Col>
-        <Col xs={12} md={6} className="text-center" data-aos="fade-left">
+        <Col xs={12} md={6} className="text-center my-2" data-aos="fade-left">
           <img src="/hi.jpg" alt="Jeya Chandran" className="img-fluid rounded-circle float-animation" />
         </Col>
       </Row>
